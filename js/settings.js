5789b790cc3c7b4df85e3211104c41084efc4136
ThadkaKeerthika <tonnynoddy987@gmail.com>
2022-05-13 17:27:57 +0530

    changes on settings.js & settings. html

--- js/settings.js
+++ js/settings.js
@@ -1,27 +1,25 @@
 function update_password(){
-
-    let email_input = $('#emailIdInput').val()
+    let email_input = sessionStorage.getItem("emailId")
     let old_password = $("#oldpasswordInput").val()
     let new_password = $("#newpasswordInput").val()
-
-
-
-
-    if((email_input.length === 0)||(old_password.length === 0)||(new_password.length === 0)){
+    let confirm_password = $('#confirmPasswordInput').val()
+    if((!email_input)||(!old_password)||(!new_password)||(!confirm_password)){
         alert("Enter all the fields")
         return
     }
+    if(confirm_password != new_password){
+        alert("Confirm Password field is different from new password field");
+        return;
+    }
     // new password must be different from old password
-
     if(old_password === new_password){
         alert("New password must be different from old password")
+        return;
     }
-
     data = {
         "emailId":email_input
     }
     //validating email
-
     const validateEmail = (email) => {
         return String(email)
           .toLowerCase()
@@ -29,12 +27,9 @@ function update_password(){
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
           );
       };
-
       if(!validateEmail(email_input)){
         alert("Enter Valid mail");
       }
-
-
     // updating the password
     token = sessionStorage.getItem("jwt")
     data = {
@@ -50,20 +45,14 @@ function update_password(){
         data: JSON.stringify(data),
         headers: {
             Authorization: 'Bearer '+token
-
         },
-
         success: function success(data){
-
            alert(data);
         },
-
         error: function error(data){
             console.log("unable to reach the website, error : ", data);
         }
     })
-
 }
-
 update_btn = document.getElementById("update_button");
 update_btn.addEventListener("click", update_password);
