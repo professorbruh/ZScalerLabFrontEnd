a9661461fc7603c2e1f61ec4f6efc8634fc696b8
Sidharth Rajagopal <62765220+professorbruh@users.noreply.github.com>
2022-05-16 17:27:33 +0530

    transfer modal

--- js/transfer.js
+++ js/transfer.js
@@ -55,12 +55,26 @@ function transact(amount){
     })
 }
 function success1(){
-  Swal.fire("Congratulations", "Registration successful", "success");
+
+  // Swal.fire("Congratulations", "Transfer successful", "success");
+  Swal.fire({
+    position: 'top-center',
+    icon: 'success',
+    title: 'Transaction successful',
+    showConfirmButton: false,
+    timer: 1500
+  }).then(()=>{
+      $('#closebutton').click();
+      window.location.href="/home.html";
+  })
+
 }
 function transfer(){
     var receiver = document.getElementById("account_number").value;
     var sender = sessionStorage.getItem("emailId")
     var amount = document.getElementById("amount").value;
+    sessionStorage.setItem("receiver",receiver);
+    sessionStorage.setItem("amount",amount);
     Swal.fire({
       title: 'Password is required for transaction:',
       input: 'password',
@@ -91,15 +105,17 @@ function transfer(){
           Authorization: 'Bearer ' + sessionStorage.getItem("jwt")        
         },
         success: function (data) {
-          Swal.fire({
-            position: 'top-center',
-            icon: 'success',
-            title: 'Transaction Successful',
-            showConfirmButton: false,
-            timer: 1500
-          }).then(()=>{
-            console.log(data)
-          })
+          // Swal.fire({
+          //   position: 'top-center',
+          //   icon: 'success',
+          //   title: 'Transaction Successful',
+          //   showConfirmButton: false,
+          //   timer: 1500
+          // }).then(()=>{
+          //   console.log(data)
+          // })
+          $("#exampleModalCenter").modal('show');
+          console.log("We are in transaciton success");
         },
         error: function (data) {
           Swal.fire({
@@ -116,4 +132,70 @@ function transfer(){
       })
       }
 })
+// window.location.href="/transfer1.html";
+$("#button1").click();
 }
+
+function failure()
+{
+   var sender = sessionStorage.getItem("emailId")
+   var receiver = sessionStorage.getItem("receiver",receiver);
+   var amount = sessionStorage.getItem("amount",amount);
+   var transferData={
+    
+       "sender":sender,
+       "receiver":receiver ,
+       "amount":amount
+  }
+  // Swal.fire({
+  //   position: 'top-center',
+  //   icon: 'success',
+  //   title: 'Transaction successful',
+  //   showConfirmButton: false,
+  //   timer: 1500
+  // }).then(()=>{
+  //     $('#closebutton').click();
+  //     window.location.href="/home.html";
+  // })
+  $.ajax(
+    {
+     type: "POST",
+     url: BASE_URL + "transaction/reversal",
+     contentType: "application/json",
+     data: JSON.stringify(transferData),
+     dataType: "json",
+     headers:{
+       Authorization: 'Bearer ' + sessionStorage.getItem("jwt")        
+     },
+     success:function(data)
+     {
+       Swal.fire({
+          icon: 'Error',
+          title: 'Transaction Failed',
+          footer: "Payment gateway status returned failed"
+       }).then(()=>{
+          $('#closebutton').click();
+          window.location.href="/home.html";
+       })
+
+       }
+     ,
+     error:function(data)
+     {
+      Swal.fire({
+        icon: 'Error',
+        title: 'Reversal Failed',
+        footer: "Payment gateway status returned failed"
+
+     }).then(()=>{
+      $('#closebutton').click();
+      window.location.href="/home.html";
+   })
+     }
+    }
+  )
+   }
+   
+
+
+
