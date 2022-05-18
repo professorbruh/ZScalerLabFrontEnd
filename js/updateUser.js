168c2cd53b7887c229d41f25b8569548fb98e6bb
Sidharth Rajagopal <62765220+professorbruh@users.noreply.github.com>
2022-05-18 16:09:28 +0530

    Changes to profile.html and updateUser

--- js/updateUser.js
+++ js/updateUser.js
@@ -1,24 +1,31 @@
+BASE_URL = "http://localhost:7080/"
 function updateUser()
 {  
     var updateData = {
-        "emailId" : sessionStorage.getItem("emailId"),
-        "password": document.getElementById("password"),
-        "address": document.getElementById("address"),
-        "phoneNumber":document.getElementById("phoneNumber"),
-        "name" : document.getElementById("name")
+        "emailId" : sessionStorage.getItem("email_Id"),
+        "password": document.getElementById("password").value,
+        "address": document.getElementById("address").value,
+        "phoneNumber":document.getElementById("phone_number").value,
+        "name" : document.getElementById("name").value
+        
     }
     
-    
+    console.log(updateData);
     $.ajax({
         type: "POST",
-        url: BASE_URL + "customers/createCustomer",
+        url: BASE_URL + "customers/updateUser",
         contentType: "application/json",
         data: JSON.stringify(updateData),
-        dataType: "json",
+        headers: {
+            Authorization: "Bearer " + sessionStorage.getItem("jwt"),
+            },
+
         success:function(data){
-            
+            console.log(data);
+        },
+        error: function(data){
+            console.log(data);
         }
-        
 
     })
 }
